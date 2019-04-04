28774d7c86a49aba3c14185931315f0183015cc2
apiwat.h <apiwat.h@aware.co.th>
2019-04-04 10:22:04 +0700

    Add more components.

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { NewsFeedComponent } from './news-feed/news-feed.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     NewsFeedComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
