c06abc06a5a44242b6003d799aaa1d9f021771c1
apiwat.h <apiwat.h@aware.co.th>
2019-04-04 09:59:12 +0700

    Prepare components.

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    NewsFeedComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
