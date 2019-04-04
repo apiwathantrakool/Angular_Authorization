1176b8ad1dde53a81c65a38a4c4c74fe8ff2784f
apiwat.h <apiwat.h@aware.co.th>
2019-04-04 10:22:16 +0700

    Update Nav.

--- ng-app/src/app/app-routing.module.ts
+++ ng-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'newsFeed', component: NewsFeedComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
